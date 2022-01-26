a56cbf6e2752b846d79402b4495b1e7a2275aebd
Philipp Chapkovski <chapkovski@gmail.com>
2022-01-26 20:30:06 +0300

    upd info vue cmp

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -7,38 +7,39 @@
         height="100px"
       >
         <div class="font-weight-bold mx-1 mr-3">Market A value:</div>
-        <div class='d-flex flex-row'>
-         <v-sheet outlined class="d-flex align-center rounded-l-xl pa-2 ">
-          <div class="d-flex align-center  ">
-            In assets:
-            <div
-              class="ml-1 pa-2 primary   white--text text-no-wrap rounded-pill"
-            >
-              ${{ stock_value_by_market("A") }}
+        <div class="d-flex flex-row">
+          <v-sheet outlined class="d-flex align-center rounded-l-xl pa-2 ">
+            <div class="d-flex align-center  ">
+              In assets:
+              <div
+                class="ml-1 pa-2 primary   white--text text-no-wrap rounded-pill"
+              >
+                ${{ stock_value_by_market("A").toFixed(2) }}
+              </div>
             </div>
-          </div>
-        </v-sheet>
-        <v-sheet outlined class="d-flex align-center ml-1 pa-2">
-          <div class="d-flex align-center  ">
-            In cash:
-            <div
-              class="ml-1 pa-2 primary   white--text text-no-wrap rounded-pill"
-            >
-              ${{ market_A.cash }}
-            </div>
-          </div>
-        </v-sheet>
-        <v-sheet outlined class="d-flex align-center ml-1 pa-2 rounded-r-xl">
-          <div class="d-flex align-center  font-weight-bold ">
-            Total:
-            <div class="ml-1 pa-2 red   white--text text-no-wrap rounded-pill">
-              ${{ total_in_market("A") }}
-            </div>
-          </div>
-        </v-sheet>
-    
-        </div>
           </v-sheet>
+          <v-sheet outlined class="d-flex align-center ml-1 pa-2">
+            <div class="d-flex align-center  ">
+              In cash:
+              <div
+                class="ml-1 pa-2 primary   white--text text-no-wrap rounded-pill"
+              >
+                ${{ market_A.cash.toFixed(2) }}
+              </div>
+            </div>
+          </v-sheet>
+          <v-sheet outlined class="d-flex align-center ml-1 pa-2 rounded-r-xl">
+            <div class="d-flex align-center  font-weight-bold ">
+              Total:
+              <div
+                class="ml-1 pa-2 red   white--text text-no-wrap rounded-pill"
+              >
+                ${{ total_in_market("A").toFixed(2) }}
+              </div>
+            </div>
+          </v-sheet>
+        </div>
+      </v-sheet>
       <v-sheet
         elevation="3"
         class="d-flex pa-3 align-center rounded-sm flex-column mx-3"
@@ -47,33 +48,35 @@
         <div class="font-weight-bold mx-1 mr-3">Market B value:</div>
         <div class="d-flex flex-row">
           <v-sheet outlined class="d-flex align-center rounded-l-xl pa-2 ">
-          <div class="d-flex align-center  ">
-            In assets:
-            <div
-              class="ml-1 pa-2 primary   white--text text-no-wrap rounded-pill"
-            >
-              ${{ stock_value_by_market("B") }}
+            <div class="d-flex align-center  ">
+              In assets:
+              <div
+                class="ml-1 pa-2 primary   white--text text-no-wrap rounded-pill"
+              >
+                ${{ stock_value_by_market("B").toFixed(2) }}
+              </div>
             </div>
-          </div>
-        </v-sheet>
-        <v-sheet outlined class="d-flex align-center ml-1 pa-2">
-          <div class="d-flex align-center  ">
-            In cash:
-            <div
-              class="ml-1 pa-2 primary   white--text text-no-wrap rounded-pill"
-            >
-              ${{ market_B.cash }}
+          </v-sheet>
+          <v-sheet outlined class="d-flex align-center ml-1 pa-2">
+            <div class="d-flex align-center  ">
+              In cash:
+              <div
+                class="ml-1 pa-2 primary   white--text text-no-wrap rounded-pill"
+              >
+                ${{ market_B.cash.toFixed(2) }}
+              </div>
             </div>
-          </div>
-        </v-sheet>
-        <v-sheet outlined class="d-flex align-center ml-1 pa-2 rounded-r-xl">
-          <div class="d-flex align-center  font-weight-bold ">
-            Total:
-            <div class="ml-1 pa-2 red   white--text text-no-wrap rounded-pill">
-              ${{ total_in_market("B") }}
+          </v-sheet>
+          <v-sheet outlined class="d-flex align-center ml-1 pa-2 rounded-r-xl">
+            <div class="d-flex align-center  font-weight-bold ">
+              Total:
+              <div
+                class="ml-1 pa-2 red   white--text text-no-wrap rounded-pill"
+              >
+                ${{ total_in_market("B").toFixed(2) }}
+              </div>
             </div>
-          </div>
-        </v-sheet>
+          </v-sheet>
         </div>
       </v-sheet>
     </div>
@@ -92,7 +95,7 @@
             <div
               class="ml-1 pa-2 primary   white--text text-no-wrap rounded-pill"
             >
-              ${{ total_stock_value() }}
+              ${{ total_stock_value().toFixed(2) }}
             </div>
           </div>
         </v-sheet>
@@ -102,7 +105,7 @@
             <div
               class="ml-1 pa-2 primary   white--text text-no-wrap rounded-pill"
             >
-              ${{ total.cash }}
+              ${{ total.cash.toFixed(2) }}
             </div>
           </div>
         </v-sheet>
@@ -110,7 +113,7 @@
           <div class="d-flex align-center  font-weight-bold ">
             Total:
             <div class="ml-1 pa-2 red   white--text text-no-wrap rounded-pill">
-              ${{ total_in_both_markets() }}
+              ${{ total_in_both_markets().toFixed(2) }}
             </div>
           </div>
         </v-sheet>
@@ -133,9 +136,7 @@ export default {
     ...mapState(["socket", "total", "market_A", "market_B", "merged"]),
   },
 
-  methods: {
-  
-  },
+  methods: {},
 };
 </script>
 
